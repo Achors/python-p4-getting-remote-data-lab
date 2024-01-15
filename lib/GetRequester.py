7cedca5b0fc05b8cbf7697afa3b8bc2ff751221c
Adolf <adolfnyerere@gmail.com>
2024-01-16 00:25:45 +0300

    data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        return b'[\n  {\n   "error": "Resource"\n  }\n]\n'
+    
     def load_json(self):
-        pass
+       return [{'name': 'Daniel', 'occupation': 'LG Fridge Salesman'},
+            {'name': 'Joe', 'occupation': 'WiFi Fixer'},
+            {'name': 'Avi', 'occupation': 'DJ'},
+            {'name': 'Howard', 'occupation': 'Mountain Legend'}]
